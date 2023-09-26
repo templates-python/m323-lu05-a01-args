d4408131b54947df79f7d1414aee1431cbad4efa
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-09-26 11:09:08 +0200

    done

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 def multiply_all(*args):
-    # TODO: Implementiere die Funktion
-    pass
+    """
+    Multiplies all the given numbers together and returns the product.
+
+    Parameters:
+        *args (float or int): Variable number of arguments to be multiplied.
+
+    Returns:
+        float or int: The product of all the given numbers.
+    """
+    product = 1
+    for num in args:
+        product *= num
+    return product
 
 
 if __name__ == '__main__':
