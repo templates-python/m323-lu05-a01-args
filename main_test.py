70e78b49fa81643469053e36bb40e8492e001019
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:06:20 +0200

    Ran pyink on branch main

--- main_test.py
+++ main_test.py
@@ -1,13 +1,19 @@
 from main import multiply_all
 
+
 def test_multiply_all_no_args():
     assert multiply_all() == 1, "Should return 1 when no arguments are passed"
 
+
 def test_multiply_all_single_arg():
-    assert multiply_all(3) == 3, "Should return the same number when only one argument is passed"
+    assert (
+        multiply_all(3) == 3
+    ), "Should return the same number when only one argument is passed"
+
 
 def test_multiply_all_multiple_args():
     assert multiply_all(1, 2, 3, 4) == 24, "Should return the product of all arguments"
+
 
 def test_multiply_all_with_zero():
     assert multiply_all(1, 2, 0, 4) == 0, "Should return 0 if any of the arguments is 0"
